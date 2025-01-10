4b80e4b4ca061791f3fdcca553eabee0c65347fa
valentin.zhuravel <valentin.zhuravel@x5.ru>
2025-01-10 15:31:16 +0300

    homework05 done абстрагировался от конкретных банкнот

--- L12-solid/src/main/java/ru/otus/homework/AtmImpl.java
+++ L12-solid/src/main/java/ru/otus/homework/AtmImpl.java
@@ -1,59 +1,52 @@
 package ru.otus.homework;
 
-import static ru.otus.homework.Banknote.FIVE_HUNDRED;
-import static ru.otus.homework.Banknote.ONE_HUNDRED;
-import static ru.otus.homework.Banknote.ONE_THOUSAND;
-
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AtmImpl implements ATM {
-  private static final Logger log = LoggerFactory.getLogger(AtmImpl.class);
-  private final MoneyStorage moneyStorage;
+    private static final Logger log = LoggerFactory.getLogger(AtmImpl.class);
+    private final MoneyStorage moneyStorage;
 
-  public AtmImpl(MoneyStorage moneyStorage) {
-    this.moneyStorage = moneyStorage;
-  }
-
-  @Override
-  public void depositCache(Map<Banknote, Integer> banknotesToCount) {
-    moneyStorage.addMoney(banknotesToCount);
-  }
-
-  @Override
-  public Map<Banknote, Integer> withdrawMoney(int requiredAmountOfMoney) {
-    return moneyStorage.withdrawMoney(generateBanknoteMapByAmount(requiredAmountOfMoney));
-  }
-
-  @Override
-  public Map<Banknote, Integer> showBalance() {
-    log.info("balance info:\n{} ", moneyStorage.toString());
-    return moneyStorage.getFullBalance();
-  }
-
-  private Map<Banknote, Integer> generateBanknoteMapByAmount(int amount) {
-    if (amount % ONE_HUNDRED.getNominal() != 0) {
-      throw new IssufficientAmountException(
-          String.format("Запрошенное количество не кратно %d", ONE_HUNDRED.getNominal()));
+    public AtmImpl(MoneyStorage moneyStorage) {
+        this.moneyStorage = moneyStorage;
     }
-    int remains = amount;
-    Map<Banknote, Integer> banknoteResultMap = new HashMap<>();
-    if (remains >= ONE_THOUSAND.getNominal()) {
-      int banknotesCount = remains / ONE_THOUSAND.getNominal();
-      remains = remains - (banknotesCount * ONE_THOUSAND.getNominal());
-      banknoteResultMap.put(ONE_THOUSAND, banknotesCount);
+
+    @Override
+    public void depositCache(Map<Banknote, Integer> banknotesToCount) {
+        moneyStorage.addMoney(banknotesToCount);
     }
-    if (remains >= FIVE_HUNDRED.getNominal()) {
-      int banknotesCount = remains / FIVE_HUNDRED.getNominal();
-      remains = remains - (banknotesCount * FIVE_HUNDRED.getNominal());
-      banknoteResultMap.put(FIVE_HUNDRED, banknotesCount);
+
+    @Override
+    public Map<Banknote, Integer> withdrawMoney(int requiredAmountOfMoney) {
+        return moneyStorage.withdrawMoney(generateBanknoteMapByAmount(requiredAmountOfMoney));
     }
-    if (remains >= ONE_HUNDRED.getNominal()) {
-      int banknotesCount = remains / ONE_HUNDRED.getNominal();
-      banknoteResultMap.put(ONE_HUNDRED, banknotesCount);
+
+    @Override
+    public Map<Banknote, Integer> showBalance() {
+        log.info("balance info:\n{} ", moneyStorage.toString());
+        return moneyStorage.getFullBalance();
     }
-    return banknoteResultMap;
-  }
+
+    private Map<Banknote, Integer> generateBanknoteMapByAmount(int amount) {
+        Map<Banknote, Integer> banknoteResultMap = new HashMap<>();
+        int remains = amount;
+        boolean canBeDivided =
+                Arrays.stream(Banknote.values()).anyMatch(banknote -> amount % banknote.getNominal() == 0);
+        if (!canBeDivided) {
+            throw new IssufficientAmountException(
+                    String.format("Запрошенное количество %d не может быть разделено на купюры!",
+                            amount));
+        }
+        for (Banknote banknote : Banknote.values()) {
+            if (remains >= banknote.getNominal()) {
+                int banknotesCount = remains / banknote.getNominal();
+                remains = remains - (banknotesCount * banknote.getNominal());
+                banknoteResultMap.put(banknote, banknotesCount);
+            }
+        }
+        return banknoteResultMap;
+    }
 }
